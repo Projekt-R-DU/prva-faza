c5fc617b15b2f3678c90ea77bb5c35248339f10b
Anto <anto.matanovic9@gmail.com>
2021-12-14 15:30:12 +0100

    cleanup

--- CenteredLoss.py
+++ CenteredLoss.py
@@ -1,15 +1,18 @@
-import torch
 import numpy as np
 
+"""
+    Implementation: Make class instance before training or every time before starting a new epoch.        
+"""
 
 class CenterLoss:
-    def __init__(self):
+    def __init__(self, x, y):
         self.has_params = False
+        self.centers = self.center(x, y)
 
-    def centers(self, x, y):
-        return np.sum(x * y, axis=0, keepdims=True) / np.sum(y, axis=0)
+    def center(self, x, y):
+        return np.sum(x * y, axis=0, keepdims=True) / np.sum(y, axis=0, keepdims=True)
 
-    def forward(self, x, y):
+    def forward(self, x):
         """
     Args:
       x: ndarray of shape (N, num_classes).
@@ -17,9 +20,9 @@ class CenterLoss:
     Returns:
       Scalar, average loss over N examples.
     """
-        return (1 / 2) * np.sum((x - self.centers(x, y)) ** 2)
+        return (1 / 2) * np.sum((x - self.centers) ** 2)
 
-    def backward_inputs(self, x, y):
+    def backward_inputs(self, x):
         """
     Args:
       x: ndarray of shape (N, num_classes).
@@ -27,4 +30,4 @@ class CenterLoss:
     Returns:
         ndarray of shape (N, num_classes).
     """
-        return x - self.centers(x, y)
+        return x - self.centers
